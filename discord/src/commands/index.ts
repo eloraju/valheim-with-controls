bb558a49407c28b733eb40fe709720a3023e8dcc
Juuso Elo-Rauta <juuso.er@gmail.com>
2022-12-07 01:17:51 +0200

    added two command stubs

--- discord/src/commands/index.ts
+++ discord/src/commands/index.ts
@@ -1,9 +1,13 @@
 import { testCommand } from "./test";
 import {DiscordCommandWithHandler} from "../types";
 import {statusCommand} from "./status";
+import {startCommand} from "./start";
+import {stopCommand} from "./stop";
 const commands = new Map<string, DiscordCommandWithHandler>([
   [testCommand.name, testCommand],
-  [statusCommand.name, statusCommand]
+  [statusCommand.name, statusCommand],
+  [startCommand.name, startCommand],
+  [stopCommand.name, stopCommand]
 ]);
 
 export default commands;
---
+++ discord/src/commands/stop.ts
@@ -0,0 +1,19 @@
+import {DiscordCommandType, DiscordCommandWithHandler, DiscordInteraction, DiscordInteractionResponse,} from "../types";
+import {InteractionResponseType} from "discord-interactions";
+import axios from 'axios';
+
+export const stopCommand: DiscordCommandWithHandler = {
+  name: "stop",
+  type: DiscordCommandType.CHAT_INPUT,
+  description: "Stops the server if there are no players",
+  handler: async function testHandler(interaction: DiscordInteraction): Promise<DiscordInteractionResponse> {
+    const res = await axios.get("https://api.chucknorris.io/jokes/random")
+    const joke = res.data.value
+    return {
+      type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+      data: {
+        content: `Not implemented yet. Meanwhile you can enjoy this Chuck Norris joke. ${joke}`
+      }
+    }
+  }
+}
