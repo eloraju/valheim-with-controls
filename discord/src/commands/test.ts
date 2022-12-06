25779f049a081c195549e20ba5eb06b158c95328
Juuso Elo-Rauta <juuso.er@gmail.com>
2022-12-07 00:52:13 +0200

    added a fallback to user.username if there is no nick to be found

--- discord/src/commands/test.ts
+++ discord/src/commands/test.ts
@@ -9,7 +9,7 @@ export const testCommand: DiscordCommandWithHandler = {
     return {
       type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
       data: {
-        content: `This is a test function. Stop calling it. That means you ${interaction.member?.nick}`
+        content: `This is a test function. Stop calling it. That means you ${interaction.member?.nick || interaction.member?.user?.username}`
       }
     }
   }
